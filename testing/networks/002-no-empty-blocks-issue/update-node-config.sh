b31a417a4c1dd2598df361e14b2747a17dc6cf22
Thane Thomson <connect@thanethomson.com>
2019-02-05 16:07:57 +0200

    Fix broken sed command execution (cross-platform)

--- testing/networks/002-no-empty-blocks-issue/update-node-config.sh
+++ testing/networks/002-no-empty-blocks-issue/update-node-config.sh
@@ -5,7 +5,7 @@ NODE_DOMAIN=${NODE_DOMAIN:-"sredev.co"}
 
 for CFG_FILE in $(find /tmp/nodes -name 'config.toml'); do
     NODE_ID=$(basename $(dirname $(dirname ${CFG_FILE})))
-    sed -i'' \
+    sed -i.orig \
         -e "s/t\([ioa]\)k\([0-9]*\):/t\1k\2.${NODE_DOMAIN}:/g" \
         -e "s/^proxy_app = \(.*\)$/proxy_app = \"kvstore\"/" \
         -e "s/^moniker = \(.*\)$/moniker = \"${NODE_ID}\"/" \
