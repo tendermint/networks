27d0e0052aefe18f1e1b02305b346b0d72d32820
Thane Thomson <connect@thanethomson.com>
2019-02-19 05:33:45 +0200

    Add dynamic actor message parsing from JSON

--- pkg/actor/messages.go
+++ pkg/actor/messages.go
@@ -1,6 +1,9 @@
 package actor
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+)
 
 // MessageType allows us to restrict the types of messages actors can send to
 // one another.
@@ -13,11 +16,35 @@ const (
 	PoisonPill MessageType = "poison-pill"
 )
 
+type baseMessage struct {
+	Type MessageType `json:"type"`           // What type of message is this?
+	Data interface{} `json:"data,omitempty"` // The data/contents of the message, if any.
+}
+
 // Message encapsulates a message that can be sent between actors.
 type Message struct {
 	Type   MessageType // What type of message is this?
 	Data   interface{} // The data/contents of the message, if any.
 	Sender Actor       // The actor that sent the message.
+}
+
+// MessageParser is a callback function that allows us to parse a specific
+// message type.
+type MessageParser func(data json.RawMessage) (interface{}, error)
+
+// MessageParserRegistry keeps track of the various different message types' parsers.
+type MessageParserRegistry struct {
+	Parsers map[MessageType]MessageParser
+}
+
+var messageParserRegistry = &MessageParserRegistry{
+	Parsers: make(map[MessageType]MessageParser),
+}
+
+func init() {
+	RegisterMessageParser(Ping, ParseMessageWithNoData)
+	RegisterMessageParser(Pong, ParseMessageWithNoData)
+	RegisterMessageParser(PoisonPill, ParseMessageWithNoData)
 }
 
 func (m *Message) String() string {
@@ -31,4 +58,46 @@ func (m *Message) Error() error {
 		return err
 	}
 	return nil
+}
+
+// ParseJSONMessage will attempt to parse the given JSON string as a message.
+func ParseJSONMessage(s string) (*Message, error) {
+	var raw json.RawMessage
+	m := baseMessage{
+		Data: &raw,
+	}
+	if err := json.Unmarshal([]byte(s), m); err != nil {
+		return nil, err
+	}
+	parser, ok := messageParserRegistry.Parsers[m.Type]
+	if !ok {
+		return nil, fmt.Errorf("Unrecognized message type: %s", m.Type)
+	}
+	d, err := parser(raw)
+	if err != nil {
+		return nil, err
+	}
+	return &Message{Type: m.Type, Data: d}, nil
+}
+
+// ToJSON is a utility function that assists in converting the message to a JSON
+// string.
+func (m *Message) ToJSON() (string, error) {
+	data, err := json.Marshal(baseMessage{Type: m.Type, Data: m.Data})
+	if err != nil {
+		return "", err
+	}
+	return string(data), nil
+}
+
+// RegisterMessageParser registers (adds or overrides) the parser for the given
+// message type.
+func RegisterMessageParser(mt MessageType, p MessageParser) {
+	messageParserRegistry.Parsers[mt] = p
+}
+
+// ParseMessageWithNoData assumes that the Message.Data field is of no
+// consequence, and thus returns nil for the data field.
+func ParseMessageWithNoData(data json.RawMessage) (interface{}, error) {
+	return nil, nil
 }
