a2c38bab0758ed68e53f485f6ba79fb6c66e17a2
mircea-c <mircea@tendermint.com>
2019-03-07 20:58:57 -0500

    dockerfile, sim start and notification scripts v1

--- testnet-kitchen/src/simulation/docker/gaia_simulation/multisim_ecs.sh
+++ testnet-kitchen/src/simulation/docker/gaia_simulation/multisim_ecs.sh
@@ -1,10 +1,16 @@
-#!/bin/bash -x
+#!/bin/bash
 
-go test ./cosmos-sdk/cmd/gaia/app -run TestFullGaiaSimulation \
-                                  -SimulationEnabled=true \
-                                  -SimulationNumBlocks=$BLOCKS \
-                                  -SimulationVerbose=true \
-                                  -SimulationCommit=true \
-                                  -SimulationSeed=$SEED \
-                                  -SimulationPeriod=$PERIOD \
-                                  -v -timeout 24h
+go test ./cmd/gaia/app -run TestFullGaiaSimulation \
+                       -SimulationEnabled=true \
+                       -SimulationNumBlocks=${BLOCKS} \
+                       -SimulationVerbose=true \
+                       -SimulationCommit=true \
+                       -SimulationSeed=${SEED} \
+                       -SimulationPeriod=${PERIOD} \
+                       -v -timeout 24h
+
+if [[ $? -ne 0 ]] && [[ -z "${SLACK_URL}" ]]; then
+    go run notify_slack.go "${SLACK_URL}" 1
+else
+    go run notify_slack.go "${SLACK_URL}" 0
+fi
