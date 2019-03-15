c4909d68b081d5604f6c6929212e82b5772858bd
Thane Thomson <connect@thanethomson.com>
2019-03-15 12:03:16 -0400

    Fix/simplify load testing framework (#18)
    Fix/simplify load testing framework by creating a `loadtest.Run` method that executes the entire framework, including command line parameter/configuration parsing and master/slave execution.
    
    This also updates the documentation on how to make use of the load testing framework as a developer.

--- cmd/tm-load-test/main.go
+++ cmd/tm-load-test/main.go
@@ -1,63 +1,9 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"os"
-
-	"github.com/sirupsen/logrus"
 	"github.com/tendermint/networks/pkg/loadtest"
 )
 
 func main() {
-	var (
-		isMaster   = flag.Bool("master", false, "start this process in MASTER mode")
-		isSlave    = flag.Bool("slave", false, "start this process in SLAVE mode")
-		configFile = flag.String("c", "load-test.toml", "the path to the configuration file for a load test")
-		isVerbose  = flag.Bool("v", false, "increase logging verbosity to DEBUG level")
-	)
-	flag.Usage = func() {
-		fmt.Println(`Tendermint load testing utility
-
-tm-load-test is a tool for distributed load testing on Tendermint networks,
-assuming that your Tendermint network currently runs the "kvstore" proxy app.
-
-Usage:
-  tm-load-test -c load-test.toml -master   # Run a master
-  tm-load-test -c load-test.toml -slave    # Run a slave
-
-Flags:`)
-		flag.PrintDefaults()
-		fmt.Println("")
-	}
-	flag.Parse()
-
-	if (!*isMaster && !*isSlave) || (*isMaster && *isSlave) {
-		fmt.Println("Either -master or -slave is expected on the command line to explicitly specify which mode to use.")
-		os.Exit(1)
-	}
-
-	if *isVerbose {
-		logrus.SetLevel(logrus.DebugLevel)
-	}
-	logger := logrus.WithField("ctx", "main")
-
-	var err error
-
-	if *isMaster {
-		logger.Infoln("Starting in MASTER mode")
-		err = loadtest.RunMaster(*configFile)
-	} else {
-		logger.Infoln("Starting in SLAVE mode")
-		err = loadtest.RunSlave(*configFile)
-	}
-
-	if err != nil {
-		logger.WithError(err).Errorln("Load test execution failed")
-		if ltErr, ok := err.(*loadtest.Error); ok {
-			os.Exit(int(ltErr.Code))
-		} else {
-			os.Exit(1)
-		}
-	}
+	loadtest.Run()
 }
--- pkg/loadtest/runners.go
+++ pkg/loadtest/runners.go
@@ -1,12 +1,71 @@
 package loadtest
 
 import (
+	"flag"
+	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
 
+	"github.com/sirupsen/logrus"
 	"github.com/tendermint/networks/pkg/loadtest/messages"
 )
+
+// Run must be executed from your `main` function in your Go code. This can be
+// used to fast-track the construction of your own load testing tool for your
+// Tendermint ABCI application.
+func Run() {
+	var (
+		isMaster   = flag.Bool("master", false, "start this process in MASTER mode")
+		isSlave    = flag.Bool("slave", false, "start this process in SLAVE mode")
+		configFile = flag.String("c", "load-test.toml", "the path to the configuration file for a load test")
+		isVerbose  = flag.Bool("v", false, "increase logging verbosity to DEBUG level")
+	)
+	flag.Usage = func() {
+		fmt.Println(`Tendermint load testing utility
+
+tm-load-test is a tool for distributed load testing on Tendermint networks,
+assuming that your Tendermint network currently runs the "kvstore" proxy app.
+
+Usage:
+  tm-load-test -c load-test.toml -master   # Run a master
+  tm-load-test -c load-test.toml -slave    # Run a slave
+
+Flags:`)
+		flag.PrintDefaults()
+		fmt.Println("")
+	}
+	flag.Parse()
+
+	if (!*isMaster && !*isSlave) || (*isMaster && *isSlave) {
+		fmt.Println("Either -master or -slave is expected on the command line to explicitly specify which mode to use.")
+		os.Exit(1)
+	}
+
+	if *isVerbose {
+		logrus.SetLevel(logrus.DebugLevel)
+	}
+	logger := logrus.WithField("ctx", "main")
+
+	var err error
+
+	if *isMaster {
+		logger.Infoln("Starting in MASTER mode")
+		err = RunMaster(*configFile)
+	} else {
+		logger.Infoln("Starting in SLAVE mode")
+		err = RunSlave(*configFile)
+	}
+
+	if err != nil {
+		logger.WithError(err).Errorln("Load test execution failed")
+		if ltErr, ok := err.(*Error); ok {
+			os.Exit(int(ltErr.Code))
+		} else {
+			os.Exit(1)
+		}
+	}
+}
 
 // RunMaster will build and execute a master node for load testing and will
 // block until the testing is complete or fails.
