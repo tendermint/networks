773abf62751c86948ee84375960a503ab4262930
Thane Thomson <connect@thanethomson.com>
2019-05-02 10:29:28 -0400

    Make status checker script generic to allow for different domain names

--- scripts/load-testing/networks/001-reference/scripts/check-status.sh
+++ scripts/load-testing/networks/001-reference/scripts/check-status.sh
@@ -3,6 +3,8 @@
 EXIT_CODE=0
 SLEEP=${SLEEP:-3}
 
+NODE_DOMAIN=${NODE_DOMAIN:-".sredev.co"}
+
 if (( ${SLEEP} > 0 )); then
     echo "Waiting a few seconds for Tendermint network to start up..."
     sleep ${SLEEP}
@@ -13,7 +15,7 @@ echo "Checking network status (assumes node configuration files are all in /tmp/
 # Run through each node and check its status to see if it's up
 for CFG_FILE in $(find /tmp/nodes -name 'config.toml'); do
     NODE_ID=$(basename $(dirname $(dirname ${CFG_FILE})))
-    if curl -s -m 3 "http://${NODE_ID}.sredev.co:26657/status" > /dev/null; then
+    if curl -s -m 3 "http://${NODE_ID}${NODE_DOMAIN}:26657/status" > /dev/null; then
         echo "${NODE_ID} is UP"
     else
         echo "${NODE_ID} is DOWN"
